21b6d445654efe89c1ec18ba747828c1b7686fab
Matheus Henrique da Silva <matheeus.silva9@hotmail.com>
2024-04-28 11:26:16 -0300

    first commit

---
+++ funcoes.php
@@ -0,0 +1,18 @@
+<?php
+
+    function exibirData(){
+        echo "Hoje é dia ".date("d");
+    }
+
+    function somar($n1, $n2){
+        return $n1 + $n2;
+    }
+    function positivoNegativo($v){
+        if($v > 0){
+            return "Valor positivo";
+        }else if($v < 0){
+            return "Valor negativo";
+        }else{
+            return "Igual a zero";
+        }
+    }
